541060fe694e828d7f25596c3f77ce4a40170bce
Patka879 <mysiak.patrycja@gmail.com>
2024-05-06 21:33:00 +0200

    removing http headers

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -3,15 +3,7 @@ import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Room } from './room'
-
-const httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Headers': '*'
-    })
-  };
+ 
 
   @Injectable({
     providedIn: 'root'
@@ -26,23 +18,22 @@ const httpOptions = {
     constructor(private http:HttpClient) { }
 
     getRooms(): Observable<Room[]> {
-        return this.http.get<Room[]>(this.getRoomsLink, httpOptions)
+        return this.http.get<Room[]>(this.getRoomsLink)
     }
 
     addRoom(room : Room): Observable<any> {
     return this.http.post (
         this.createRoomUrl,
         room,
-        httpOptions
         )
     }
 
     deleteRoom(arg0: number): Observable<any> {
-        return this.http.delete(this.deleteUrl + arg0, httpOptions)
+        return this.http.delete(this.deleteUrl + arg0)
     }
 
     updateRoom(room: Room): Observable<any> {
-      return this.http.patch(this.updateUrl + room.id, room, httpOptions);
+      return this.http.patch(this.updateUrl + room.id, room);
     }
 
   }
